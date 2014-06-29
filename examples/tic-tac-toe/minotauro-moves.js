7201ecd37ddb66a1edc61900e0d133929b500c02
David Robles <drobles@gmail.com>
2014-06-29 08:04:40 -0700

    Create MoveHistoryModel

---
+++ examples/tic-tac-toe/minotauro-moves.js
@@ -0,0 +1,16 @@
+var Minotauro = Minotauro || {};
+
+Minotauro.MoveHistoryView = function(options) {
+    this.controller.options
+    this.el = document.createElement("table");
+};
+
+Minotauro.MoveHistoryView.prototype = {
+
+    constructor: Minotauro.MoveHistoryView,
+
+    render: function() {
+
+    }
+
+};
---
+++ examples/tic-tac-toe/move-history-model.js
@@ -0,0 +1,11 @@
+var Minotauro = Minotauro || {};
+
+Minotauro.MoveHistoryModel = function() {
+
+};
+
+Minotauro.MoveHistoryModel.prototype = {
+
+    constructor: Minotauro.MoveHistoryModel
+
+};
