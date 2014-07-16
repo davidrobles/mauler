6e5dfd258d80a72856924de3847e0b13b3d41bf6
David Robles <drobles@gmail.com>
2014-07-15 21:39:55 -0700

    Move UCB algorithm to mcts.js

--- src/players/mcts.js
+++ src/players/mcts.js
@@ -106,3 +106,58 @@ mauler.players.MCTSNode.prototype = {
     }
 
 };
+
+///////////////////
+// Tree policies //
+///////////////////
+
+mauler.players.UCB1 = function(options) {
+    options = options || {};
+    this.c = options.c; // TODO add random number generator
+};
+
+mauler.players.UCB1.prototype = {
+
+    constructor: mauler.players.UCB1,
+
+    move: function(node, player) {
+        var bestMove = -1,
+            max = node.game.curPlayer() === player,
+            bestValue = max ? -Number.MAX_VALUE : Number.MAX_VALUE,
+            nb = 0;
+        for (var move = 0; move < node.game.numMoves(); move++) {
+            nb += node.actionCount(move);
+        }
+        for (move = 0; move < node.game.numMoves(); move++) {
+            var value = 0;
+
+            // ensures that each arm is selected once before further exploration
+            if (node.actionCount(move) === 0)
+            {
+                var bias = (Math.random() * 1000) + 10;
+                value = max ? (100000000 - bias) : (-100000000 + bias); // TODO: refactor
+            }
+            else
+            {
+                var exploitation = node.actionValue(move);
+                var exploration = this.c * Math.sqrt(Math.log(nb) / node.actionCount(move));
+                value += exploitation;
+                value += max ? exploration : -exploration;
+            }
+
+            if (max)
+            {
+                if (value > bestValue) {
+                    bestMove = move;
+                    bestValue = value;
+                }
+            }
+            else if (value < bestValue) { // min
+                bestMove = move;
+                bestValue = value;
+            }
+        }
+        return bestMove;
+    }
+
+};
