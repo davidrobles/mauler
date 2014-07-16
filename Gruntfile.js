bac6a83e0ab46600b837782d78967e6334a3034d
David Robles <drobles@gmail.com>
2014-07-15 20:45:34 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,5 +49,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-clean");
 
+    grunt.registerTask("default", ["build"])
     grunt.registerTask("build", [ "concat" ]);
 };
