7ec7cfd60ab6fc5ab9dba9a01cf84d9b6d56246a
David Robles <drobles@gmail.com>
2014-07-19 15:02:31 -0700

    Create file for canvas player

--- src/games/tic-tac-toe/canvas.js
+++ src/games/tic-tac-toe/canvas.js
@@ -1,5 +1,5 @@
 (function() {
-    mauler.games.tic = mauler.tic || {};
+    mauler.games.tic = mauler.games.tic || {};
 
     mauler.games.tic.CanvasView = function(options) {
         this.model = options.model;
@@ -162,69 +162,4 @@
 
     };
 
-    mauler.games.tic.CanvasPlayer = function(options) {
-        this.moveChosen = null;
-        this.match = options.match;
-        this.canvasView = options.canvasView;
-        this.canvas = options.canvasView.canvas;
-        this.addListeners();
-    };
-
-    var CanvasPlayer = mauler.games.tic.CanvasPlayer;
-
-    CanvasPlayer.prototype = {
-
-        constructor: CanvasPlayer,
-
-        move: function() {
-            if (!this.moveChosen) {
-                throw new Error("No move chosen!");
-            }
-            var selMove = this.moveChosen;
-            this.moveChosen = null;
-            return selMove;
-        },
-
-        // Listeners
-
-        addListeners: function() {
-            this.addClickListener();
-            this.addMouseMoveListener();
-            this.addMouseOutListener();
-        },
-
-        addClickListener: function () {
-            this.canvas.addEventListener("click", function(event) {
-                var canvasLoc = mauler.utils.windowToCanvas(this.canvas, event.clientX, event.clientY);
-                var move = this.canvasView.canvasLocationToMove(canvasLoc);
-                var moves = this.match.curGame().moves();
-                if (_.contains(moves, move)) {
-                    this.moveChosen = move;
-                    this.match.next();
-                    this.canvasView.render(); // TODO Move somewhere else?
-                    // TODO add trigger()?
-                }
-            }.bind(this));
-        },
-
-        addMouseMoveListener: function () {
-            this.canvas.addEventListener("mousemove", function(event) {
-                var canvasLoc = mauler.utils.windowToCanvas(this.canvas, event.clientX, event.clientY);
-                var move = this.canvasView.canvasLocationToMove(canvasLoc);
-                var moves = this.match.curGame().moves();
-                if (_.contains(moves, move)) {
-                    this.canvasView.highlightedMoves = [move];
-                    this.canvasView.render();
-                }
-            }.bind(this));
-        },
-
-        addMouseOutListener: function () {
-            this.canvas.addEventListener("mouseout", function() {
-                this.canvasView.highlightedMoves = [];
-                this.canvasView.render();
-            }.bind(this));
-        }
-
-    };
 }());
