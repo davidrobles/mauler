8664348cfbaf266ed19155bd22adf8b1d821e423
David Robles <drobles@gmail.com>
2014-07-19 14:49:24 -0700

    Wrap mauler.utils in an IIFE

--- src/utils.js
+++ src/utils.js
@@ -1,73 +1,83 @@
-mauler.utils.argMax = function(outcomes) {
-    var maxArg = 0,
-        maxValue = outcomes[0];
-    for (var i = 1; i < outcomes.length; i++) {
-        if (outcomes[i] > maxValue) {
-            maxArg = i;
-            maxValue = outcomes[i];
+(function() {
+
+    mauler.utils = mauler.utils || {};
+
+    mauler.utils = {
+
+        argMax: function(outcomes) {
+            var maxArg = 0,
+                maxValue = outcomes[0];
+            for (var i = 1; i < outcomes.length; i++) {
+                if (outcomes[i] > maxValue) {
+                    maxArg = i;
+                    maxValue = outcomes[i];
+                }
+            }
+            return maxArg;
+        },
+
+        playRandomGame: function(game) {
+            console.log(game.toString());
+            while (!game.isOver()) {
+                game.move();
+                console.log(game.toString());
+            }
+        },
+
+        playNGames: function(game, players, numGames) {
+            var stats = {
+                oneWins: 0,
+                twoWins: 0,
+                draws: 0
+            };
+            for (var i = 0; i < numGames; i++) {
+                var newGame = game.copy();
+                while (!newGame.isOver()) {
+                    var curPlayer = players[newGame.curPlayer()];
+                    var move = curPlayer.move(newGame);
+                    newGame.move(move);
+                }
+                var outcomes = newGame.outcomes();
+                if (outcomes[0] === 'WIN') {
+                    stats.oneWins++;
+                } else if (outcomes[1] === 'WIN') {
+                    stats.twoWins++;
+                } else {
+                    stats.draws++;
+                }
+            }
+            return stats;
+        },
+
+        windowToCanvas: function(canvas, x, y) {
+            var bbox = canvas.getBoundingClientRect();
+            return {
+                x: x - bbox.left * (canvas.width / bbox.width),
+                y: y - bbox.top * (canvas.height / bbox.height)
+            };
+        },
+
+        UtilFunc: function(options) {
+            options = options || {};
+            this.win  = options.win  ||  1.0;
+            this.draw = options.draw ||  0.0;
+            this.loss = options.loss || -1.0;
         }
-    }
-    return maxArg;
-};
 
-mauler.utils.playRandomGame = function(game) {
-    console.log(game.toString());
-    while (!game.isOver()) {
-        game.move();
-        console.log(game.toString());
-    }
-};
-
-mauler.utils.playNGames = function(game, players, numGames) {
-    var stats = {
-        oneWins: 0,
-        twoWins: 0,
-        draws: 0
     };
-    for (var i = 0; i < numGames; i++) {
-        var newGame = game.copy();
-        while (!newGame.isOver()) {
-            var curPlayer = players[newGame.curPlayer()];
-            var move = curPlayer.move(newGame);
-            newGame.move(move);
-        }
-        var outcomes = newGame.outcomes();
-        if (outcomes[0] === 'WIN') {
-            stats.oneWins++;
-        } else if (outcomes[1] === 'WIN') {
-            stats.twoWins++;
-        } else {
-            stats.draws++;
-        }
-    }
-    return stats;
-};
 
-mauler.utils.windowToCanvas = function(canvas, x, y) {
-    var bbox = canvas.getBoundingClientRect();
-    return {
-        x: x - bbox.left * (canvas.width / bbox.width),
-        y: y - bbox.top * (canvas.height / bbox.height)
+    mauler.utils.UtilFunc.prototype.eval = function(game, player) {
+        if (game.isOver()) {
+            var outcomes = game.outcomes();
+            switch (outcomes[player]) {
+                case "WIN":
+                    return this.win;
+                case "DRAW":
+                    return this.draw;
+                case "LOSS":
+                    return this.loss;
+            }
+        }
     };
-};
 
-mauler.utils.UtilFunc = function(options) {
-    options = options || {};
-    this.win  = options.win  ||  1.0;
-    this.draw = options.draw ||  0.0;
-    this.loss = options.loss || -1.0;
-};
-
-mauler.utils.UtilFunc.prototype.eval = function(game, player) {
-    if (game.isOver()) {
-        var outcomes = game.outcomes();
-        switch (outcomes[player]) {
-            case "WIN":
-                return this.win;
-            case "DRAW":
-                return this.draw;
-            case "LOSS":
-                return this.loss;
-        }
-    }
-};
+}());
