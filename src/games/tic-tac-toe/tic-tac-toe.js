8eac26f1e7caaa735052427e37f420a4ba4c5405
David Robles <drobles@gmail.com>
2014-12-11 09:05:29 -0800

    Rename bitboardMoves()

--- src/games/tic-tac-toe/tic-tac-toe.js
+++ src/games/tic-tac-toe/tic-tac-toe.js
@@ -54,13 +54,13 @@
             if (move < 0 || move >= moves.length) {
                 throw new RangeError('Illegal move');
             }
-            this.setCurrentBitboard(this.getCurrentBitboard() | (1 << this.legalMoves()[move]));
+            this.setCurrentBitboard(this.getCurrentBitboard() | (1 << this.bitboardMoves()[move]));
             return this;
         },
 
         moves: function() {
             var mvs = [],
-                legal = this.legalMoves();
+                legal = this.bitboardMoves();
             for (var i = 0; i < legal.length; i++) {
                 var row = Math.floor(legal[i] / 3);
                 var col = (legal[i] % 3) + 1;
@@ -159,7 +159,7 @@
             return count;
         },
 
-        legalMoves: function() {
+        bitboardMoves: function() {
             var moves = [];
             if (!this.isWin()) {
                 var legal = ~(this.crosses | this.noughts);
