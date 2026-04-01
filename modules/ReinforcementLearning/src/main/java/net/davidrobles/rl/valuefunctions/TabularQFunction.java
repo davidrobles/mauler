06dc6ce66b90c1d1b710752a5b028da808752719
David Robles <drobles@gmail.com>
2026-03-31 22:14:47 -0700

    Fix raw types in TabularQFunction; fix typo in doc

--- modules/ReinforcementLearning/src/main/java/net/davidrobles/rl/valuefunctions/TabularQFunction.java
+++ modules/ReinforcementLearning/src/main/java/net/davidrobles/rl/valuefunctions/TabularQFunction.java
@@ -4,24 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 import net.davidrobles.rl.QPair;
 
-/** Tabular (lookup table) implementation of a state-sction value function. */
+/** Tabular (lookup table) implementation of a state-action value function. */
 public class TabularQFunction<S, A> implements QFunction<S, A> {
-    private Map<QPair, Double> actionValues = new HashMap<QPair, Double>();
+    private Map<QPair<S, A>, Double> actionValues = new HashMap<>();
 
     @Override
     public double getValue(S state, A action) {
-        return getValue(new QPair<S, A>(state, action));
+        return getValue(new QPair<>(state, action));
     }
 
-    private double getValue(QPair qPair) {
+    private double getValue(QPair<S, A> qPair) {
         return actionValues.containsKey(qPair) ? actionValues.get(qPair) : 0;
     }
 
-    public void setValue(QPair qPair, double value) {
+    public void setValue(QPair<S, A> qPair, double value) {
         actionValues.put(qPair, value);
     }
 
     public void setValue(S state, A action, double value) {
-        setValue(new QPair<S, A>(state, action), value);
+        actionValues.put(new QPair<>(state, action), value);
     }
 }
