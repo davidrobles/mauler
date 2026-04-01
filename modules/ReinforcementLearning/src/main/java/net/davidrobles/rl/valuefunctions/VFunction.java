9848914ef990831d6b1744f9919b2970c1a0497c
David Robles <drobles@gmail.com>
2026-03-31 22:33:48 -0700

    Remove redundant public modifier from VFunction interface method
    Interface methods are implicitly public; the explicit keyword is noise.

--- modules/ReinforcementLearning/src/main/java/net/davidrobles/rl/valuefunctions/VFunction.java
+++ modules/ReinforcementLearning/src/main/java/net/davidrobles/rl/valuefunctions/VFunction.java
@@ -3,5 +3,5 @@ package net.davidrobles.rl.valuefunctions;
 /** A state value function. */
 public interface VFunction<S> {
     /** Returns the estimated value of the given state. */
-    public double getValue(S state);
+    double getValue(S state);
 }
